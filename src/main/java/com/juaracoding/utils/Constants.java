c70b466b9ee52cde5c79b0df2353d0d559028682
Aldi Triavin <aldi3avin@gmail.com>
2023-10-12 01:05:27 +0700

    create utils package (global variable)

---
+++ src/main/java/com/juaracoding/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.juaracoding.utils;
+
+public class Constants {
+    // Global Variable
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+    public static final long TIMEOUT = 10;
+    public static final long SORTDELAY = 2;
+    public static final long LONGDELAY = 5;
+    public static final String USERNAME = "Admin";
+    public static final String PASSWORD = "admin123";
+}
